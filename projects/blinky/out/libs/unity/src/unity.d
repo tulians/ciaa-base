73d347756947ba91376125ec88f63fea45309be9
Julian Ailan <julianailan@outlook.com>
2026-03-09 18:51:19 +0000

    Refactor include management and add host-based unit testing

---
+++ projects/blinky/out/libs/unity/src/unity.d
@@ -0,0 +1,2 @@
+projects/blinky/out/libs/unity//src/unity.o: libs/unity//src/unity.c \
+ libs/unity//inc/unity.h libs/unity//inc/unity_internals.h
---
+++ projects/blinky/out/projects/blinky/src/logic.d
@@ -0,0 +1,2 @@
+projects/blinky/out/projects/blinky/src/logic.o: \
+ projects/blinky/src/logic.c projects/blinky/inc/logic.h
